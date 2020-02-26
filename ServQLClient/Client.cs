13c2b6c90f15cfb97af278ceb61652a485a44f92
Alberto Ruiz <albertzruiz1@outlook.es>
2020-02-26 21:04:23 +0100

    changed framework to dotNet Core

---
+++ ServQLClient/Client.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace SQLiteCLIENT
+{
+    public class Client
+    {
+        Connection connection { get; set; }
+        string[] resultType = { "OK", "ERROR", "FILE" };
+        public bool isLoged { get; set; }
+
+        public Package.Response? Login(string user,string password)
+        {
+            if (connection.isReady)
+            {
+                Package.Login loginPackage = new Package.Login();
+                loginPackage.User = user;
+                loginPackage.Password = password;
+                connection.Send(JsonSerializer.Serialize(loginPackage));
+                
+                Package.Response response = JsonSerializer.Deserialize<Package.Response>(connection.Recv());
+                if (response.Result == resultType[0]) isLoged = true;
+                return response;
+            }
+            return null;
+        }
+
+        public string[] splitOut(string OutD)
+        {
+            string[] result = OutD.Split(':');
+            if (result.Length <= 1) throw (new Exception("Invalid string"));
+            string[] Message = new string[result.Length - 1];
+            Message = result.Skip(1).ToArray();
+            result = new string[2] { result[0], string.Join(" ",Message) };
+            return result;
+        }
+
+        public Func<string, Package.Response> NewDb, DelDb, Query,OpenDb;
+        public Func<Package.Response> GetDBs,CloseDb,GetTables;
+
+        public Client(Connection connection)
+        {
+            this.connection = connection;
+            NewDb = arg => sendCommand("new", arg);
+            DelDb = arg => sendCommand("del", arg);
+            Query = arg => sendCommand("exec", arg);
+            OpenDb = arg => sendCommand("open", arg); 
+
+            GetTables = () => sendCommand("exec", $"SELECT name FROM sqlite_master WHERE type = 'table'");
+            CloseDb = () => sendCommand("close", "");
+            GetDBs = () => sendCommand("list", "");
+
+        }
+        public bool TestCon()
+        {
+            if (!this.connection.isReady) throw (new Exception("Connection not Ready"));
+            if (!this.connection.isLoged) throw (new Exception("Connection isnt Logged"));
+            Package.Response response = sendCommand("test");
+            if (response.Result == "OK" && response.Message == "OK") return true;
+            return false;
+            
+        }
+        public Package.Response sendCommand(string cmd, string args = "")
+        {
+            try
+            {
+                Package.Response response;
+                string RAWrequest,RAWresponse;
+                Package.Request requestPackage = new Package.Request();
+                requestPackage.Command = cmd;
+                requestPackage.Args = args.Split();
+                RAWrequest = JsonSerializer.Serialize(requestPackage) + "\n";
+                connection.Send(RAWrequest);
+                RAWresponse = connection.Recv();
+                response = JsonSerializer.Deserialize<Package.Response>(RAWresponse);
+                if (response.Result == resultType[0]) return response;
+                else return null;
+
+            }
+            catch (Exception E)
+            {
+                return null;
+            }
+        }
+        public List<List<string>> formatTable(Package.Response response)
+        {
+            List<List<string>> table = new List<List<string>>();
+            
+
+            foreach (string protoRow in response.Data)
+            {
+                List<string> Row = protoRow.Split(',').ToList<string>();
+                table.Add(Row);
+            }
+
+            return table;
+        }
+
+        public  static void getFile(string path,byte[] fileBytes)
+        {
+            string[] pathList = path.Split('/');
+            path = pathList[pathList.Length - 1];
+            File.Create(path);
+            File.WriteAllBytes(path, fileBytes);
+
+        }
+
+        public class Package
+        {
+            [Serializable]
+            public class Request
+            {
+                public String Command { get; set; }
+                public String[] Args { get; set; }
+                public String Hash { get; set; }
+
+            }
+
+            [Serializable]
+            public class Response
+            {
+                public String Result { get; set; }
+                public String Message { get; set; }
+                public String[] Data { get; set; }
+            }
+
+            public Request GenPackage(string command, String[] args, String hash)
+            {
+                Request package = new Request();
+                package.Command = command;
+                package.Args = args;
+                package.Hash = hash;
+                return package;
+            }
+
+            [Serializable]
+            public class Login
+            {
+                public int Type { get; set; }
+                public String version { get; set; }
+                public String User { get; set; }
+                public String Password { get; set; }
+
+            }
+        }
+
+    }
+}
---
+++ ServQLClient/Connection.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SQLiteCLIENT
+{
+    
+    public class Connection
+    {
+        public string ip { get; set; }
+        public int Port = 124;
+        public string clientVersion = "0.30b";
+        TcpClient client { get; set; }
+        public SslStream stream { get; set; }
+        public bool isReady = false;
+        public bool isLoged = false;
+
+        string version { get; set; }
+
+        public  Connection(string ip)
+        {
+            this.ip = ip;
+        }
+
+        public TcpClient Open()
+        {
+            TcpClient client = new TcpClient(this.ip, this.Port);
+            this.client = client;
+            stream = new SslStream(client.GetStream(),false,new RemoteCertificateValidationCallback(ValidateServerCertificate),null);
+            stream.AuthenticateAsClient(ip);
+            this.isReady = true;
+            stream.Flush();
+            return client;
+        }
+
+
+        
+
+        public static bool ValidateServerCertificate(object sender, X509Certificate certificate,
+        X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+        
+            return true;
+        }
+
+        public string GetVersion()
+        {
+            if (version == null)
+            {
+                if (stream != null)
+                {
+                    version = Recv();
+                    return version;
+                }
+            }
+            return null;
+        }
+
+        
+
+        public void Close(TcpClient client = null)
+        {
+            if (client == null)
+            {
+                if (this.client == null) return;
+                client = this.client;
+            }
+            client.Close();
+            isReady = false;
+            isLoged = false;
+            version = null;
+            stream = null;
+
+        }
+       
+        public void Send(string cmd)
+        {
+            if (!isReady) throw (new Exception("Connection not Ready"));
+            if (version == null) GetVersion();
+            byte[] data = Encoding.UTF8.GetBytes('\u0001' + cmd + '\n');
+            stream.Write(data,0,data.Length);            
+        }
+       
+
+        public string Recv()
+        {
+            if (!isReady) throw (new Exception("Connection not Ready"));
+            string result;
+            byte[] dataResult = new byte[client.ReceiveBufferSize];
+            stream.Read(dataResult, 0, dataResult.Length);
+            stream.Flush();
+            result = Encoding.UTF8.GetString(dataResult);
+            result = result.TrimEnd('\0');
+            result = result.TrimEnd('\n');
+            if (result.Length == 0) return "";
+            if (result[0] == '\u0001' && result.Length == 1)
+            {
+                result = null;
+                dataResult = new byte[client.ReceiveBufferSize];
+                stream.Read(dataResult, 0, dataResult.Length);
+                result = Encoding.UTF8.GetString(dataResult);
+                result = result.TrimEnd('\0');
+                result = result.TrimEnd('\n');
+            }
+            else if (result[0] == '\u0001')
+            {
+                result = result.TrimStart('\u0001');
+            }
+            return result;
+
+        }
+
+        public byte[] Recv(int byteLength) //TODO: update to accept telomer
+        {
+            if (!isReady) throw (new Exception("Connection not Ready"));
+            byte[] dataRecv = new byte[byteLength];
+            stream.Read(dataRecv, 0, dataRecv.Length);
+            stream.Flush();
+
+            return dataRecv;
+        }
+
+    }
+
+}
